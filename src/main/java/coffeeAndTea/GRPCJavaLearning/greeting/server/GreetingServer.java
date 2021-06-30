fddbb61122b0fdba66be7974c3417b6b51ba49cc
wei xue <jimmy.wei.xue@gmail.com>
2021-06-30 16:15:54 -0500

    reflection

--- src/main/java/coffeeAndTea/GRPCJavaLearning/greeting/server/GreetingServer.java
+++ src/main/java/coffeeAndTea/GRPCJavaLearning/greeting/server/GreetingServer.java
@@ -2,6 +2,7 @@ package coffeeAndTea.GRPCJavaLearning.greeting.server;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,21 +14,23 @@ public class GreetingServer {
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Hello GRPC");
 
-//        Server server = ServerBuilder
-//                .forPort(50051)
-//                .addService(new GreetServiceImpl())
-//                .addService(new CalculatorServerImpl())
-//                .build();
-
         Server server = ServerBuilder
                 .forPort(50051)
                 .addService(new GreetServiceImpl())
                 .addService(new CalculatorServerImpl())
-                .useTransportSecurity(
-                        new File("ssl/server.crt"),
-                        new File("ssl/server.pem")
-                )
+                .addService(ProtoReflectionService.newInstance())
                 .build();
+//
+//        Server server = ServerBuilder
+//                .forPort(50051)
+//                .addService(new GreetServiceImpl())
+//                .addService(new CalculatorServerImpl())
+//                .addService(ProtoReflectionService.newInstance())
+//                .useTransportSecurity(
+//                        new File("ssl/server.crt"),
+//                        new File("ssl/server.pem")
+//                )
+//                .build();
         server.start();
 
         Runtime.getRuntime().addShutdownHook(new Thread(() ->
